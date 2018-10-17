8709f3d2be7db1c4ccf3adb5d3e1ebc3ecdbc9c5
David Malý <malyce.dvd@gmail.com>
2018-10-17 11:37:18 +0200

    Added Enemy

--- PongSample/MainWindow.xaml.cs
+++ PongSample/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
@@ -27,11 +28,12 @@ namespace PongSample
             var width = this.Width;
             var height = this.Height;
             var left = width / 2 - ((double)Platform.GetValue(Canvas.WidthProperty) / 2);
+           
             Canvas.SetLeft(Platform, left);
-
+            Canvas.SetLeft(Enemy, left);
             var top = height - ((double)Platform.GetValue(Canvas.HeightProperty)) - 50;
             Canvas.SetTop(Platform, top);
-
+            Canvas.SetTop(Enemy, 0);
             timer2.Interval = new TimeSpan(100);
             timer2.Tick += (s, args) => MovePlatform(2, direction);
 
@@ -39,6 +41,14 @@ namespace PongSample
             timer.Interval = new TimeSpan(1000);
             timer.Tick += new EventHandler(KeyPressed);
             timer.Start();
+
+
+            DispatcherTimer timer3 = new DispatcherTimer();
+            timer3.Interval = new TimeSpan(50000);
+            timer3.Tick += (s, args) => MoveEnemy( );
+            timer3.Start();
+
+            
         }
 
         int repeats = 5;
@@ -49,8 +59,7 @@ namespace PongSample
         Direction direction = Direction.Right;
         void KeyPressed(object source, EventArgs e)
         {  
-  
-            
+           
             bool keyPressed = false;
 
             if (Keyboard.IsKeyDown(Key.Left))
@@ -93,5 +102,32 @@ namespace PongSample
                 actualRepeat = 0;
             }
         }
+
+        public void MoveEnemy()
+        { 
+            double top = (double)Enemy.GetValue(Canvas.TopProperty);
+            Canvas.SetTop(Enemy, top + 1);
+            CheckCollision();
+        }
+
+        public void CheckCollision()
+        {
+            double topE = (double)Enemy.GetValue(Canvas.TopProperty);
+            double leftE = (double)Enemy.GetValue(Canvas.LeftProperty);
+
+            double topP = (double)Platform.GetValue(Canvas.TopProperty);
+            double leftP = (double)Platform.GetValue(Canvas.LeftProperty);
+
+            double sizeE = Enemy.Width;
+
+            if (topE >= 300)
+            {
+                if( leftP >= leftE - sizeE && leftP <= leftE + sizeE)
+                {
+                    MessageBox.Show("hit");
+                }
+            }
+        }
+
     }
 }
